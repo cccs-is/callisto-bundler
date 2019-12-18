e56e1537d0ef5c5c97feca1232fdf6585eb1b09c
besedin <oleg.besedin@165gc.onmicrosoft.com>
2019-12-18 09:59:53 -0500

    Cleanup token passing

--- callisto_bundler/helper.py
+++ callisto_bundler/helper.py
@@ -8,22 +8,20 @@ ENDPOINT_POST_SHARE = '/shared/'
 
 logger = logging.getLogger(__name__)
 
+
 def share(handler, model):
     base_url = os.getenv('JUPYTER_CALLISTO_URL', 'http://127.0.0.1:5000/').rstrip('/')
-    #logger.info('>> in bundler -> request.headers -> access_token:', handler.request.headers.get("X-Access-Token"))
-    #logger.info('>> callisto_bundler -> base_url:' +  base_url)
-    # access_token = os.getenv('NOTEBOOK_ACCESS_TOKEN', '')
-    access_token = handler.request.headers.get('X-Access-Token')
+    access_token = os.getenv('NOTEBOOK_ACCESS_TOKEN')
+    if not access_token:
+        access_token = handler.request.headers.get('X-Access-Token')
     try:
         notebook_filename = model['name']
         notebook_content = nbformat.writes(model['content']).encode('utf-8')
         data = {'notebook_name': notebook_filename, 'notebook_contents': notebook_content}
 
         post_url = base_url + ENDPOINT_UPLOAD
-        #logger.info('>> callisto_bundler -> post_url:' + post_url)
-  
-        headers={'Authorization': 'Token ' + access_token,
-                 'X-Access-Token': access_token}
+
+        headers = {'Authorization': 'Bearer ' + access_token}
         response = requests.post(url=post_url, headers=headers, data=data, timeout=5.0)
 
         if response.status_code != 200:
