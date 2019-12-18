cfc22533217209c5be5c45fd73f0828cd84edcc2
besedin <oleg.besedin@165gc.onmicrosoft.com>
2019-12-18 15:10:21 -0500

    Forward authentication

--- callisto_bundler/helper.py
+++ callisto_bundler/helper.py
@@ -6,6 +6,10 @@ import logging
 ENDPOINT_UPLOAD = '/nbupload/'
 ENDPOINT_POST_SHARE = '/shared/'
 
+# This is the cookie name used by OAuth2 proxy
+# By default it is '_oauth2_proxy' and can be changed using '-cookie-name' config on the OAuth2 proxy.
+OAUTH_COOKIE_NAME = '_oauth2_proxy'
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,6 +18,11 @@ def share(handler, model):
     access_token = os.getenv('NOTEBOOK_ACCESS_TOKEN')
     if not access_token:
         access_token = handler.request.headers.get('X-Access-Token')
+
+    oauth_cookies = None
+    if handler.request.cookies is not None and OAUTH_COOKIE_NAME in handler.request.cookies:
+        oauth_cookies = {OAUTH_COOKIE_NAME: handler.request.cookies[OAUTH_COOKIE_NAME].value}
+
     try:
         notebook_filename = model['name']
         notebook_content = nbformat.writes(model['content']).encode('utf-8')
@@ -22,7 +31,7 @@ def share(handler, model):
         post_url = base_url + ENDPOINT_UPLOAD
 
         headers = {'Authorization': 'Bearer ' + access_token}
-        response = requests.post(url=post_url, headers=headers, data=data, timeout=5.0)
+        response = requests.post(url=post_url, headers=headers, cookies=oauth_cookies, data=data, timeout=5.0)
 
         if response.status_code != 200:
             # TODO handle token refresh
